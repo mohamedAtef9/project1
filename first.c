4190cc73ef3f6248cfdd92046fa00784e0e88bef
mohamedAtef9 <mohamedatef50645>
2020-11-07 10:58:31 +0200

    haha

---
+++ first.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main ()
+{
+    float x , y , sum ;
+    printf("please inter 2 numbers\n");
+    scanf("%f%f",&x,&y);
+    sum=x+y;
+    printf("the sum is \n%f+%f=%f",x,y,sum);
+}
